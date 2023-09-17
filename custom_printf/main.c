13042303431b84e6fa25cd160b15025561a05331
Onyi13 <njokuonyi562@gmail.com>
2023-09-17 06:17:45 -0700

    DSKJF

--- custom_printf/main.c
+++ custom_printf/main.c
@@ -1,11 +1,11 @@
-#include "print_functions.h"
+#include "main.h"
 /**
 *main - Entry point for testing custom printf.
 *Return: Always 0.
 */
-int main()
-{_printf("Hello World!\n");
+int main(void)
+{_printf("Let's try to printf a simple sentence.\n");
 _printf("Character: %c\n", 'A');
 _printf("String: %s\n", "This is a string.");
-_printf("Print percentage: %%\n");
-return 0; }
+_printf("Percent: %%\n");
+return (0); }
--- custom_printf/printf_functions.c
+++ custom_printf/printf_functions.c
@@ -1,34 +1,38 @@
-#include "print_functions.h"
+#include "main.h"
 /**
-*_printf - Custom printf function.
+*_printf - Our custom printf function.
 *@format: format string.
 *Return: number of characters printed.
 */
 int _printf(const char *format, ...)
 {va_list args;
 int count = 0;
-char *str;
 va_start(args, format);
 while (*format)
 {
 if (*format == '%' && (format[1] == 'c' || format[1] == 's' || format[1] == '%'))
 {format++;
-if (*format == 'c')
+switch (*format)
+{
+case 'c':
 {char c = va_arg(args, int);
 write(1, &c, 1);
-count++;}
-else if (*format == 's')
-{str = va_arg(args, char*);
+count++;
+break; }
+case 's':
+{char *str = va_arg(args, char*);
 while (*str)
 {write(1, str, 1);
 count++;
-str++; }}
-else if (*format == '%')
+str++; }
+break; }
+case '%':
 {write(1, format, 1);
-count++; }}
+count++;
+break; }}}
 else
 {write(1, format, 1);
 count++; }
 format++; }
 va_end(args);
-return count; }
+return (count); }
