2a8a59a8019b109604a3447038fcc10131abf36b
Onyi13 <njokuonyi562@gmail.com>
2023-09-17 06:49:16 -0700

    DSFJK

---
+++ print_integer.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_integer - Print an integer as a string.
+*@args: The va_list arguments.
+*Return: The number of digits printed.
+*/
+int print_integer(va_list args)
+{int n = va_arg(args, int);
+int div = 1;
+int len = 0;
+unsigned int num;
+if (n < 0)
+{len += write(1, "-", 1);
+num = n * -1; }
+else
+num = n;
+while (num / div > 9)
+div *= 10;
+while (div != 0)
+{len += write(1, (char *)(((num / div) % 10) + '0'), 1);
+div /= 10; }
+return (len); }
--- printf_functions.c
+++ printf_functions.c
@@ -36,3 +36,23 @@ count++; }
 format++; }
 va_end(args);
 return (count); }
+/**
+*_printf - Custom printf function.
+*@format: The format string.
+*Return: The number of characters printed.
+*/
+int _printf(const char *format, ...)
+{unsigned int i = 0, count = 0;
+va_list args;
+va_start(args, format);
+while (format && format[i])
+{
+if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+{count += print_integer(args);
+i++;}
+else
+{write(1, &format[i], 1);
+count++; }
+i++; }
+va_end(args);
+return (count); }
